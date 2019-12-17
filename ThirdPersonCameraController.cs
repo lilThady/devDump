de6f40d67b1cf114c0fb76b49f344a09c4cd9062
lilThady <58400047+lilThady@users.noreply.github.com>
2019-12-17 11:33:21 +0100

    Character controller Basics

---
+++ ThirdPersonCameraController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+
+
+    public float RotationSpeed = 1f;
+    public Transform Target, Player;
+    //floats voor de muis
+    float mouseX, mouseY;
+
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+   
+  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Obstruction = Target;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+    private void LateUpdate()
+    {
+        CamControl();
+        ViewObstructed();
+    }
+
+    void CamControl()
+    {
+        
+        // muis input
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
+        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+
+        // dit zorgt er voor dat de speler niet verder met zijn muis omhoog en naar benden kan.
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+    //voor als de camera door de muur heen gaat, renderd die de muur weg.
+    void ViewObstructed()
+    {
+    //schiet een straal uit de camera 
+        RaycastHit hit;
+
+        //als er iets tussen de straal en de speler zit, dan...  
+        if (Physics.Raycast(transform.position, Target.position - transform.position, out hit, 4.5f))
+        {
+            //en als de camera de speler zijn collider niet raakt dan.. 
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                // zet obstruction var om naar een transform en zorgt er voor dat wat tussen de camera en speler zit niet wordt gerenderd.
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if (Vector3.Distance(Obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, Target.position) >= 1.5f)
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }
+
+            else
+            {
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+                if (Vector3.Distance(transform.position, Target.position) < 4.5f) 
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+                
+
+            }
+
+        }
+
+    }
+
+}
---
+++ ThirdPersonCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    //hier onder zitten alle variablen van de character controller.
+    public float Speed;
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMovement();
+
+    }
+
+    // Functie dat de speler kan bewegen over de grond heen.  
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        //een Vector punt zodat we weten hoe de character orienteerd. dus kort wat de x , y en z is.
+        // delta.time zorgt er voor dat de beweging smooth afspeelt
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+
+}      
